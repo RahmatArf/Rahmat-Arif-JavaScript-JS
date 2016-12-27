bf39b9676829332838f51b56ae7403f37bac9105
Rahmat Arif <rahmatarifibnusaimuddinhasan@gmail.com>
2016-12-28 02:51:08 +0700

    Add files via upload

---
+++ JavaScript(JS)-Time-Header-Blogger-Responsive-By-Rahmat-Arif.js
@@ -0,0 +1,53 @@
+***** JavaScript(JS) Time Header Blogger Responsive By Rahmat Arif | RahmatArifISH.blogspot.com *****
+
+<div style='background-color: #006600;border: 0px solid #00ff00;font-family: Open Sans;font-size: 13px;color: #ffffff;padding: 0px;width: 100%;text-align: center;'>
+<script type='text/javascript'>
+<!--
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var myDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth();
+var thisDay = date.getDay(),
+    thisDay = myDays[thisDay];
+var yy = date.getYear();
+var year = (yy < 1000) ? yy + 1900 : yy;
+document.write(thisDay + ', ' + day + ' ' + months[month] + ' ' + year);
+//-->
+</script>
+<div id='clock'/>
+<script type='text/javascript'>
+<!--
+function showTime() {
+    var a_p = "";
+    var today = new Date();
+    var curr_hour = today.getHours();
+    var curr_minute = today.getMinutes();
+    var curr_second = today.getSeconds();
+    if (curr_hour < 12) {
+        a_p = "AM";
+    } else {
+        a_p = "PM";
+    }
+    if (curr_hour == 0) {
+        curr_hour = 12;
+    }
+    if (curr_hour > 12) {
+        curr_hour = curr_hour - 12;
+    }
+    curr_hour = checkTime(curr_hour);
+    curr_minute = checkTime(curr_minute);
+    curr_second = checkTime(curr_second);
+ document.getElementById('clock').innerHTML=curr_hour + ":" + curr_minute + ":" + curr_second + " " + a_p;
+    }
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+setInterval(showTime, 500);
+//-->
+</script>
+</div>
